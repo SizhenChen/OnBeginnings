a39006f3caabfc35356b70572c285b6f1dc21686
SizhenChen <chens656@newschool.edu>
2023-02-14 20:49:31 -0500

    up

--- Script.js
+++ Script.js
@@ -1,3 +1,15 @@
 function fadeOut() {
-    document.getElementById("title").style.animationDuration = "1s";
+    document.getElementById("title").style.animation = "Opacity 5s";
+    document.getElementById("square").style.animation = "moveAndZoom 4s";
+    
+        $(window).load(function () {
+            window.setTimeout(function () {
+                window.location.href = "https://www.google.co.in";
+            }, 5000)
+        });
+    
 }
+
+
+
+
