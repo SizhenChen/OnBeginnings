6f4815080a53c4b374f6b971a596c8adae780d15
SizhenChen <chens656@newschool.edu>
2023-02-16 16:16:54 -0500

    up

--- Script.js
+++ Script.js
@@ -1,15 +1,14 @@
 function fadeOut() {
     document.getElementById("title").style.animation = "Opacity 5s";
     document.getElementById("square").style.animation = "moveAndZoom 4s";
-    
-        $(window).load(function () {
-            window.setTimeout(function () {
-                window.location.href = "https://www.google.co.in";
-            }, 5000)
-        });
-    
 }
 
+function Redirect() 
+{  
+    window.location="http://www.newpage.com"; 
+} 
+document.write("You will be redirected to a new page in 5 seconds"); 
+setTimeout('Redirect()', 5000);   
 
 
 
