2457e3e07bf8935de25c786fa133561d941d04db
SizhenChen <chens656@newschool.edu>
2023-02-17 21:07:09 -0500

    up

---
+++ entry1_InLife/script1.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {$("#rectangle").hide(); })
+
+
+// function zoomOut() {
+//     document.getElementById("text1").style.visibility = "hidden";
+//     $(document).ready(function() {$("#rectangle").show(); });
+//     document.getElementById("rectangle").style.animation = "Zoom 3s";   
+//     }                                                            
+
+
+
+
