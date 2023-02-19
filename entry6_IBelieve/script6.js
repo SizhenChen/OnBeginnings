4b6bf275fc125f8d92e4f28d9a5611d038d992a8
SizhenChen <chens656@newschool.edu>
2023-02-18 21:35:28 -0500

    up

--- entry6_IBelieve/script6.js
+++ entry6_IBelieve/script6.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {$("#text").hide(); })
+$(document).ready(function() {$("#shape").hide(); })
 function appear(){
     $(document).ready(function() {$("#text").show(); })
+    $(document).ready(function() {$("#shape").show(); })
 }
 setTimeout(appear,3000)
