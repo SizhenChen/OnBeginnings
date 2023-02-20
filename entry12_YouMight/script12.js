ae79f8f31b740ae0d888d8d185d00ba12f7fe5e6
SizhenChen <chens656@newschool.edu>
2023-02-19 19:33:36 -0500

    up

---
+++ entry12_YouMight/script12.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {$("#text1").hide(); })
+$(document).ready(function() {$("#text2").hide(); })
+$(document).ready(function() {$("#shape").hide(); })
+function appear(){
+    $(document).ready(function() {$("#text1").show(); })
+    $(document).ready(function() {$("#text2").show(); })
+    $(document).ready(function() {$("#shape").show(); })
+}
+setTimeout(appear,3000)
