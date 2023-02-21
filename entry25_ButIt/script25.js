c8b75beb00c3a912577881d31525c1b989703d78
SizhenChen <chens656@newschool.edu>
2023-02-21 11:19:24 -0500

    up

--- entry25_ButIt/script25.js
+++ entry25_ButIt/script25.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {$("#text").hide(); })
-$(document).ready(function() {$("#shape").hide(); })
+$(document).ready(function() {$("#shape1").hide(); })
+$(document).ready(function() {$("#shape2").hide(); })
 function appear(){
     $(document).ready(function() {$("#text").show(); })
-    $(document).ready(function() {$("#shape").show(); })
+    $(document).ready(function() {$("#shape1").show(); })
+    $(document).ready(function() {$("#shape2").show(); })
 }
 setTimeout(appear,3000)
